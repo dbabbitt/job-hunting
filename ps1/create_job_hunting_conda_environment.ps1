0d92304232737a366ba14631468e0883741ee7c1
Dave Babbitt <dave.babbitt@gmail.com>
2021-03-04 09:40:34 -0500

    Changed the environment name to conform to the new naming convention

--- ps1/create_job_hunting_conda_environment.ps1
+++ ps1/create_job_hunting_conda_environment.ps1
@@ -3,13 +3,13 @@
 # if you are deleting the environment before recreating it:
 # 
 # cd $Env:UserProfile\Documents\Repositories\job-hunting\ps1
-# clear
+# cls
 # .\create_job_hunting_conda_environment.ps1
 
 # Set up global variables
 $DisplayName = "Job Hunting"
 $RepositoryPath = "job-hunting"
-$EnvironmentName = "jh"
+$EnvironmentName = "jh_env"
 
 $HomeDirectory = $Env:UserProfile
 $EnvironmentsDirectory = "${HomeDirectory}\anaconda3\envs"
@@ -19,10 +19,10 @@ $EnvironmentPath = "${RepositoriesDirectory}\${RepositoryPath}\${EnvironmentName
 $OldPath = Get-Location
 
 # Delete environment folder
-# ."${PowerScriptsDirectory}\delete_conda_environment.ps1"
+."${PowerScriptsDirectory}\delete_conda_environment.ps1"
 
 # Create environment folder
-# ."${PowerScriptsDirectory}\create_conda_environment.ps1"
+."${PowerScriptsDirectory}\create_conda_environment.ps1"
 # ."${PowerScriptsDirectory}\update_conda_environment.ps1"
 
 # Bring up the workspace in Chrome
