2608617b17de7ed95871988966b4e449dbed56fd
Dave Babbitt <dave.babbitt@gmail.com>
2024-03-28 16:21:15 -0400

    Added encoding and decoding constants

--- py/storage.py
+++ py/storage.py
@@ -10,7 +10,10 @@
 try:
     import dill as pickle
 except:
-    import pickle
+    try:
+        import pickle5 as pickle
+    except:
+        import pickle
 import pandas as pd
 import os
 import sys
@@ -56,10 +59,21 @@ class Storage(object):
             os.makedirs(name=self.data_csv_folder, exist_ok=True)
             os.makedirs(name=self.saves_pickle_folder, exist_ok=True)
             os.makedirs(name=self.saves_csv_folder, exist_ok=True)
-
+        
         # Handy list of the different types of encodings
         self.encoding_type = ['latin1', 'iso8859-1', 'utf-8'][2]
-
+        self.encoding_types_list = ['ascii', 'cp037', 'cp437', 'cp863', 'utf_32',
+                                    'utf_32_be', 'utf_32_le', 'utf_16', 'utf_16_be',
+                                    'utf_16_le', 'utf_7', 'utf_8', 'utf_8_sig',
+                                    'latin1', 'iso8859-1', 'utf-8']
+        self.encoding_errors_list = ['ignore', 'replace', 'xmlcharrefreplace']
+        self.decoding_types_list = self.encoding_types_list.copy()
+        self.decoding_errors_list = self.encoding_errors_list.copy()
+        self.encoding_type = self.encoding_types_list[15]
+        self.encoding_error = self.encoding_errors_list[2]
+        self.decoding_type = self.decoding_types_list[15]
+        self.decoding_error = self.decoding_errors_list[0]
+    
     def csv_exists(self, csv_name, folder_path=None, verbose=False):
         if folder_path is None:
             csv_folder = self.saves_csv_folder
@@ -71,7 +85,7 @@ class Storage(object):
             print(os.path.abspath(csv_path))
 
         return os.path.isfile(csv_path)
-
+    
     def load_csv(self, csv_name=None, folder_path=None):
         if folder_path is None:
             csv_folder = self.data_csv_folder
@@ -86,15 +100,15 @@ class Storage(object):
             else:
                 csv_path = os.path.join(csv_folder, f'{csv_name}.csv')
         data_frame = pd.read_csv(os.path.abspath(csv_path), encoding=self.encoding_type)
-
+        
         return(data_frame)
 
     def pickle_exists(self, pickle_name):
         pickle_path = os.path.join(self.saves_pickle_folder, '{}.pkl'.format(pickle_name))
-
+        
         return os.path.isfile(pickle_path)
 
-    def load_dataframes(self, **kwargs):
+    def load_data_frames(self, **kwargs):
         frame_dict = {}
         for frame_name in kwargs:
             pickle_path = os.path.join(self.saves_pickle_folder, '{}.pkl'.format(frame_name))
@@ -115,7 +129,7 @@ class Storage(object):
                     frame_dict[frame_name] = self.load_csv(csv_name=frame_name, folder_path=self.saves_folder)
             else:
                 frame_dict[frame_name] = self.load_object(frame_name)
-
+        
         return frame_dict
 
     def load_object(self, obj_name, pickle_path=None, download_url=None, verbose=True):
@@ -137,7 +151,7 @@ class Storage(object):
                 self.attempt_to_pickle(object, pickle_path, raise_exception=False)
             else:
                 with open(pickle_path, 'wb') as handle:
-
+                
                     # Protocol 4 is not handled in python 2
                     if sys.version_info.major == 2:
                         pickle.dump(object, handle, 2)
@@ -148,16 +162,11 @@ class Storage(object):
                 object = pd.read_pickle(pickle_path)
             except:
                 with open(pickle_path, 'rb') as handle:
-                    try:
-                        import dill as pickle
-                        object = pickle.load(handle)
-                    except:
-                        import pickle
-                        object = pickle.load(handle)
-
+                    object = pickle.load(handle)
+        
         return(object)
 
-    def save_dataframes(self, include_index=False, verbose=True, **kwargs):
+    def save_data_frames(self, include_index=False, verbose=True, **kwargs):
         for frame_name in kwargs:
             if isinstance(kwargs[frame_name], pd.DataFrame):
                 csv_path = os.path.join(self.saves_csv_folder, '{}.csv'.format(frame_name))
@@ -169,8 +178,8 @@ class Storage(object):
     # Classes, functions, and methods cannot be pickled
     def store_objects(self, verbose=True, **kwargs):
         for obj_name in kwargs:
-            # if hasattr(kwargs[obj_name], '__call__'):
-                # raise RuntimeError('Functions cannot be pickled.')
+            if hasattr(kwargs[obj_name], '__call__'):
+                raise RuntimeError('Functions cannot be pickled.')
             pickle_path = os.path.join(self.saves_pickle_folder, '{}.pkl'.format(obj_name))
             if isinstance(kwargs[obj_name], pd.DataFrame):
                 self.attempt_to_pickle(kwargs[obj_name], pickle_path, raise_exception=False, verbose=verbose)
@@ -178,11 +187,11 @@ class Storage(object):
                 if verbose:
                     print('Pickling to {}'.format(os.path.abspath(pickle_path)))
                 with open(pickle_path, 'wb') as handle:
-
+                
                     # Protocol 4 is not handled in python 2
                     if sys.version_info.major == 2:
                         pickle.dump(kwargs[obj_name], handle, 2)
-
+                    
                     # Pickle protocol must be <= 4
                     elif sys.version_info.major == 3:
                         pickle.dump(kwargs[obj_name], handle, min(4, pickle.HIGHEST_PROTOCOL))
@@ -191,15 +200,15 @@ class Storage(object):
         try:
             if verbose:
                 print('Pickling to {}'.format(os.path.abspath(pickle_path)))
-
+        
             # Protocol 4 is not handled in python 2
             if sys.version_info.major == 2:
                 df.to_pickle(pickle_path, protocol=2)
-
+            
             # Pickle protocol must be <= 4
             elif sys.version_info.major == 3:
                 df.to_pickle(pickle_path, protocol=min(4, pickle.HIGHEST_PROTOCOL))
-
+        
         except Exception as e:
             os.remove(pickle_path)
             if verbose:
